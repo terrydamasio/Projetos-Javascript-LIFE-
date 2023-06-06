7a933542b1094ba6a02d5ababcb2837afc7785f9
Terry Damasio Santos <terry@lifecti.com.br>
2023-06-06 18:04:37 -0300

    Início do curso de javascript

---
+++ [APRENDIZADO 5] JavaScript/js/script.js
@@ -0,0 +1,4 @@
+alert('Olá, seja bem vindo ao curso!!!') // Exibe uma barra de alerta no navegador (abrir um dialog)
+
+/* seleciona um elemento no DOM e atualiza o valor desse elemento com uma string */
+document.getElementById('nome').value = 'Oi' 
